81327121ba9ad0a883b88a497bc2f0755ed9a568
PinkelPunk <PinkelPunk*******>
2015-02-09 19:42:36 +0100

    klassenerstellung

---
+++ ProjectAdventure/src/adventure/Game.java
@@ -0,0 +1,39 @@
+package adventure;
+
+public class Game
+{
+	private Raum aktuellerRaum;
+	private Held held;
+	private Parser parser;
+	
+	private void createWorld()
+	{
+		
+	}
+	
+	private void gameMenue()
+	{
+		
+	}
+	
+	private void init()
+	{
+		
+	}
+	
+	public Game()
+	{
+		
+	}
+	
+	public void goRaum(String raum)
+	{
+		
+	}
+	
+	public void raumBeschreibung()
+	{
+		
+	}
+	
+}
