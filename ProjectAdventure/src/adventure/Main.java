9fc0a96bd11fbfbb9bf044a0c2ee7e904024c3f9
anramainyu <stephanschiefer@web.de>
2015-02-10 15:38:29 +0100

    Raum Methoden geschrieben

--- ProjectAdventure/src/adventure/Main.java
+++ ProjectAdventure/src/adventure/Main.java
@@ -1,11 +1,16 @@
 package adventure;
 
+import java.util.HashMap;
+
 public class Main {
 
 	public static void main(String[] args)
 	{
-		
-		
+		HashMap gebaeude = new HashMap();
+		Raum raum1 = new Raum(gebaeude);
+		Raum raum2 = new Raum(gebaeude);
+		raum1.addAusgang(raum1,"nord");
+		raum1.getAusgang("nord");
 		
 		
 	}
--- ProjectAdventure/src/adventure/Raum.java
+++ ProjectAdventure/src/adventure/Raum.java
@@ -9,20 +9,28 @@ public class Raum
 	private String kurzBeschreibung;
 	private String langBeschreibung;
 	private String name;
-	
+
+	public Raum(HashMap<String, Raum> ausgaenge)
+	{
+		this.ausgaenge= new HashMap<String, Raum>();
+		this.ausgaenge = ausgaenge;
+	}
+
+
 	public Raum addAusgang(Raum raum, String s)
 	{
-		return null;
+		ausgaenge.put(s, raum);
+		return raum;
 	}
 	
 	public HashMap<String, Raum> getAusgaenge()
 	{
-		return null;
+		return ausgaenge;
 	}
 	
 	public Raum getAusgang(String s)
 	{
-		return null;
+		return ausgaenge.get(s);
 	}
 	
 }
