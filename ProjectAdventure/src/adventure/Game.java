055232f3ab85caf7a34db080628729057110dfb6
anramainyu <stephanschiefer@web.de>
2015-02-10 10:00:31 +0100

    Spielereingabe Name in gameMenu() erstellt und ein Objekt der Klasse Gamerstellt -> gameMenu() test

--- ProjectAdventure/src/adventure/Game.java
+++ ProjectAdventure/src/adventure/Game.java
@@ -1,5 +1,7 @@
 package adventure;
 
+import java.io.*;
+
 public class Game
 {
 	private Raum aktuellerRaum;
@@ -16,8 +18,36 @@ public class Game
 		System.out.println("-----------------------------------");
 		System.out.println("--------Punkmainyu Studios---------");
 		System.out.println("-----------------------------------");
-		System.out.println("Startmenu - [1] Start Game");
+		System.out.println("-------Member of the Chamber-------");
 
+		//eingaben lesen
+		String eingabe;
+		try
+		{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			do
+			{
+				System.out.println("Wie soll dein Held heißen?");
+			}while((eingabe=br.readLine())!=null);
+			File file = new File("/users/Spielernamen.txt");
+			if(!file.exists())
+			{
+				file.createNewFile();
+			}
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(eingabe);
+			br.close();
+		}
+		catch(IOException e)
+		{
+
+		}
+		// NumberCatch von Hasanovic
+		/*catch(NumberFormatException e)
+		{
+			System.out.println("Sie haben keine zahl eingegeben");
+		}*/
 	}
 	
 	private void init()
