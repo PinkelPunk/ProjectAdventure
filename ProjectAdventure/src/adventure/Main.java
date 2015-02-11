597d83c24712a02e5897e257901755709f9fabca
PinkelPunk <PinkelPunk******>
2015-02-11 10:46:09 +0100

    parser aktualisierung2.0

--- ProjectAdventure/src/adventure/Main.java
+++ ProjectAdventure/src/adventure/Main.java
@@ -1,9 +1,17 @@
 package adventure;
 
+import java.util.HashMap;
+
 public class Main {
 
 	public static void main(String[] args)
 	{
+		HashMap gebaeude = new HashMap();
+		Raum raum1 = new Raum(gebaeude);
+		Raum raum2 = new Raum(gebaeude);
+		raum1.addAusgang(raum1,"nord");
+		raum1.getAusgang("nord");
+		
 		
 		Game spiel1=new Game("TakeCommand");
 		
