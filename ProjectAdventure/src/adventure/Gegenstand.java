52a8bb3f74e21cb79fc38f62fe56f867068fcfe2
PinkelPunk <PinkelPunk******>
2015-02-11 10:51:18 +0100

    master aktualisierung2.0

--- ProjectAdventure/src/adventure/Gegenstand.java
+++ ProjectAdventure/src/adventure/Gegenstand.java
@@ -4,4 +4,30 @@ public class Gegenstand
 {
 	private String kurzBeschreibung;
 	private String langBeschreibung;
+	
+	public Gegenstand(String kurzBeschreibung, String langBeschreibung)
+	{
+		this.kurzBeschreibung=kurzBeschreibung;
+		this.langBeschreibung=langBeschreibung;
+	}
+
+	/**
+	 * @return the kurzBeschreibung
+	 */
+	public String getKurzBeschreibung() {
+		return kurzBeschreibung;
+	}
+
+	/**
+	 * @return the langBeschreibung
+	 */
+	public String getLangBeschreibung() {
+		return langBeschreibung;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "Kurze Beschreibung: " + kurzBeschreibung + "\nlange Beschreibung: " + langBeschreibung;
+	}
 }
